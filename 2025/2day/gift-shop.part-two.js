37c1b9a4b05ca1e488d991d813935eab562a9f2a
クロネ <ab@kurone.dev>
2025-12-09 16:36:28 +0900

    perf: 약 2.5배 개선

--- 2025/2day/gift-shop.part-two.js
+++ 2025/2day/gift-shop.part-two.js
@@ -17,14 +17,13 @@ export const partition = (string, part) => {
 
 export const isInvalidId = (id) => {
   const stringId = String(id);
-  for (let i = 2; i < stringId.length + 1; i++) {
-    const partitioningArray = partition(stringId, i);
+
+  const factors = allFactorsAbove1(stringId.length);
+  return factors.some(factor => {
+    const partitioningArray = partition(stringId, factor);
     const first = partitioningArray[0];
-    if (partitioningArray.every((it) => it === first)) {
-      return true;
-    }
-  }
-  return false;
+    return (partitioningArray.every((it) => it === first))
+  })
 };
 
 export const findInvalidIds = (range) => {
@@ -41,4 +40,6 @@ export const solve = (ranges) => {
 
 const input =
   '119-210,907313-1048019,7272640820-7272795557,6315717352-6315818282,42-65,2234869-2439411,1474-2883,33023-53147,1-15,6151-14081,3068-5955,65808-128089,518490556-518593948,3535333552-3535383752,7340190-7548414,547-889,34283147-34389716,44361695-44519217,607492-669180,7071078-7183353,67-115,969-1469,3636264326-3636424525,762682710-762831570,827113-906870,205757-331544,290-523,86343460-86510016,5536957-5589517,132876-197570,676083-793651,23-41,17920-31734,440069-593347';
+const startTime = performance.now();
 console.log(solve(input.split(',')));
+console.log('End in:', performance.now() - startTime, 'ms');
